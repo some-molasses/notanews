f8914ef40aab2657e1e0e36f61b970582698475f
some-molasses <river.stanley173@gmail.com>
2025-05-05 11:21:07 -0700

    Fetching from flask server in Next

--- api/index.py
+++ api/index.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from datetime import datetime
+from typing import TypedDict
+
 app = Flask(__name__)
 
+
+class GetArticleResponse(TypedDict):
+    author: str
+    title: str
+    date: str
+
+
 @app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def get_article() -> GetArticleResponse:
+    return {
+        "author": "molasses",
+        "title": "n recipes",
+        "date": datetime.now().isoformat(),
+    }
