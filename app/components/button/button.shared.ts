20f6aec05a5d9320db5afa711c8f4e32061bdf00
some-molasses <river.stanley173@gmail.com>
2025-05-11 10:18:01 -0700

    Another deploy fix

--- app/components/button/button.shared.ts
+++ app/components/button/button.shared.ts
@@ -1,5 +1,5 @@
 export type ButtonProps = {
   children: React.ReactNode;
-  handler: (formData: FormData) => void;
+  handler: ((formData: FormData) => void) | (() => void);
   variant?: "default" | "destructive";
 };
