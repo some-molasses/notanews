53e918730ff58c86016fbbd3bf5b67936513205f
River Stanley <river.stanley173@gmail.com>
2026-02-20 00:01:17 -0500

    Measurer iterates over all articles

--- app/dashboard/editing/[issue_id]/measurer/measurer.tsx
+++ app/dashboard/editing/[issue_id]/measurer/measurer.tsx
@@ -4,7 +4,7 @@ import { ArticleMeasurements } from "@/app/api/v2/assemble-issue/route";
 import { ArticleFrame } from "@/app/components/issue/article/article-frame";
 import { IssueFrame } from "@/app/components/issue/issue-frame";
 import { ArticleExpanded } from "@/app/utils/data-types";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./measurer.scss";
 
 const groupContentByColumn = (articleFrame: HTMLDivElement) => {
@@ -34,19 +34,52 @@ const measureArticle = (
 
 export const Measurer: React.FC<{
   articles: ArticleExpanded[];
-  setMeasurements: (measurements: Map<ArticleExpanded, number>) => void;
+  setMeasurements: (
+    measurements: Map<ArticleExpanded, ArticleMeasurements>,
+  ) => void;
 }> = ({ articles, setMeasurements }) => {
-  const [currentArticle, setCurrentArticle] = useState<ArticleExpanded>(
-    articles[0],
+  const [currentArticleIndex, setCurrentArticleIndex] = useState<number>(0);
+  const currentArticle = articles[currentArticleIndex];
+
+  const [currentMeasurements, setCurrentMeasurements] = useState(
+    new Map<ArticleExpanded, ArticleMeasurements>(),
   );
   const currentArticleRef = useRef<HTMLDivElement>(null);
 
+  // reset if articles updated
+  useEffect(() => {
+    setCurrentMeasurements(new Map());
+  }, [articles, setCurrentMeasurements]);
+
+  useEffect(() => {
+    if (!currentArticleRef.current) {
+      return;
+    }
+
+    const newMeasurement = measureArticle(
+      currentArticle.id,
+      currentArticleRef.current,
+    );
+
+    currentMeasurements.set(currentArticle, newMeasurement);
+    setCurrentMeasurements(new Map(currentMeasurements.entries()));
+    if (currentArticleIndex + 1 < articles.length) {
+      setCurrentArticleIndex(currentArticleIndex + 1);
+    } else {
+      setMeasurements(currentMeasurements);
+    }
+  }, [
+    articles.length,
+    currentArticle,
+    currentArticle.id,
+    currentArticleIndex,
+    currentMeasurements,
+    setCurrentMeasurements,
+    setMeasurements,
+  ]);
+
   if (!articles.length || !currentArticle) {
     return null;
-  }
-
-  if (currentArticleRef.current) {
-    console.log(measureArticle(currentArticle.id, currentArticleRef.current));
   }
 
   return (
